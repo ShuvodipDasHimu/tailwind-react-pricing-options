e17685ad46488cf5db14ec56d7bd1ab8cf02233e
Shuvodip Das Himu <shuvodipdashimu@gmail.com>
2025-04-15 11:26:16 +0600

    module 44-6

--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -4,14 +4,14 @@ import PricingFeature from './PricingFeature';
 const PricingCard = ({pricing}) => {
   const {name,price,description,features} = pricing;
   return (
-    <div className='border bg-blue-400 roundex-2xl p-4'>
+    <div className='flex flex-col border bg-blue-400 roundex-2xl p-4'>
       {/* card header */}
       <div>
         <h1 className="text-7xl">{name}</h1>
         <h4 className='text-3xl'>{price}</h4>
       </div>
     {/* card body */}
-    <div className='bg-blue-200 p-4 rounded-2xl mt-10'>
+    <div className='bg-blue-200 p-4 rounded-2xl mt-10 flex-1'>
       <p>{description}</p>
       {
         features.map((feature, index) => <PricingFeature
@@ -19,6 +19,7 @@ const PricingCard = ({pricing}) => {
         feature={feature}></PricingFeature>)
       }
     </div>
+    <button className='btn w-full mt-4'>Subscribe</button>
     </div>
   );
 };
--- src/components/PricingOptions/PricingOptions.jsx
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricing from '../DaisyPricing/DaisyPricing';
 
 const PricingOptions = ({pricingPromise}) => {
 
@@ -8,11 +9,18 @@ const PricingOptions = ({pricingPromise}) => {
   return (
     <div>
       <h2 className='text-5xl'>Get our Membership</h2>
-      <div className='grid md:grid-cols-3 gap-8'>
-        {
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+        {/* {
           pricingData.map(pricing => <PricingCard
             key={pricing.id}
             pricing={pricing}></PricingCard>)
+        } */}
+
+        {
+          pricingData.map(pricing=> <DaisyPricing
+          key={pricing.id}
+            pricing={pricing}
+          ></DaisyPricing>)
         }
       </div>
     </div>
